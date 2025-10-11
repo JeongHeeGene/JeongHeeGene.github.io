61c13db8061c69314aeed6d2dd877b05f538be94
JeongHeeGene <84696775+JeongHeeGene@users.noreply.github.com>
2025-10-11 11:30:46 +0900

    Add files via upload

---
+++ trumeet/js/js.js
@@ -0,0 +1,45 @@
+// let m_home = document.getElementById('m_home');
+//         function home_mouseover (){
+//             m_home.setAttribute("src","img/home_ov.png");
+//         }
+        
+//         function home_mouseleave (){
+//             m_home.setAttribute("src","img/home.png");
+//         }
+
+let m_timeM = document.getElementById('m_timeM');
+        function timeM_mouseover (){
+            m_timeM.setAttribute("src","img/timeM_ov.png");
+        }
+        
+        function timeM_mouseleave (){
+            m_timeM.setAttribute("src","img/timeM.png");
+        }
+
+let m_chatt = document.getElementById('m_chatt');
+        function chatt_mouseover (){
+            m_chatt.setAttribute("src","img/chatt_ov.png");
+        }
+        
+        function chatt_mouseleave (){
+            m_chatt.setAttribute("src","img/chatt.png");
+        }
+
+
+let m_qr = document.getElementById('m_qr');
+        function qr_mouseover (){
+            m_qr.setAttribute("src","img/qr_ov.png");
+        }
+        
+        function qr_mouseleave (){
+            m_qr.setAttribute("src","img/qr.png");
+        }
+
+let m_moment = document.getElementById('m_moment');
+        function moment_mouseover (){
+            m_moment.setAttribute("src","img/moment_ov.png");
+        }
+        
+        function moment_mouseleave (){
+            m_moment.setAttribute("src","img/moment.png");
+        }
